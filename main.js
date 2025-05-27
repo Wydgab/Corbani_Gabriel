e793b89e51ac9a37234b576611f14ac8e3104ff8
Corbani Gabriel <corbagab24@gmail.com>
2025-05-27 22:57:57 +0200

    Update main.js, add main function for calculator

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+function clearDisplay() {
+  display.value = '';
+}
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+function appendValue(value) {
+  display.value += value;
+}
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+function hideCalculator() {
+  document.getElementById('calculator').classList.remove('active');
+  document.getElementById('calculatorOverlay').classList.remove('active');
+  document.getElementById('mainContent').classList.remove('hidden');
+}
